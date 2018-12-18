42ca97dcacfab2f337f71ea51bb0422e5204ff9f
joel zulbal <joel.zulbal@satoripop.com>
2018-12-18 17:03:51 +0100

    correction

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -4,12 +4,15 @@ function createModelFile (object) {
 	var schema = object.model_name+'Schema'; 
 	var properties = ''; 
 	for (var i=0; i<object.properties.length; i++ ) {
-		var row = `${object.properties[i].property} : ${object.properties[i].type} \n`; 
-		properties = `\t\t ${properties} ${row} `; 
+		var row = `${object.properties[i].property} : ${object.properties[i].type}, \n`; 
+		properties = `${properties} ${row}`; 
 	} 
+	// var regex = /[[:punct:]]$/g; 
+	// properties = properties.replace(regex, ''); 
+	// console.log(properties);
 	var contentFile = `
-		var mongoose \t\t= require('mongoose');\n
-		var ${schema} \t\t= new mongoose.Schema({
+		var mongoose = require('mongoose');\n
+		var ${schema} = new mongoose.Schema({
 			${properties}
 		}) \n\n
 		var ${object.model_name} = mongoose.model('${object.model_name}', ${schema}); 
@@ -18,10 +21,14 @@ function createModelFile (object) {
 	fs.stat('models', (err) => {
 		if(!err) {
 			console.log('The folder already exists');
+			fs.writeFile(`models/${object.model_name}.js`, contentFile.trim(), (err) => {
+				if (err) throw err; 
+				console.log('The file was successufully created'); 
+			});
 		}
 		else if (err.code === 'ENOENT') {
         	fs.mkdirSync('models'); 
-        	fs.writeFile(`models/${object.model_name}.js`, contentFile, (err) => {
+        	fs.writeFile(`models/${object.model_name}.js`, contentFile.trim(), (err) => {
 				if (err) throw err; 
 				console.log('The file was successufully created'); 
 			}); 

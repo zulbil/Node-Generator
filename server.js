09bfeabcaeb66fff75fc0975016dc6647e480a92
Joel Alexandre Khang Zulbal <alexkhang25@yahoo.fr>
2018-12-24 11:06:46 +0100

    refactor code

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const fs            = require('fs');
-const mongoose      = require('mongoose');
+const {mongoose}    = require('./config/dbConfig');
 const commander     = require('commander');
-const inquirer      = require('inquirer');
-const bootstrap     = require('./bootstrap/bootstrap'); 
 const yargs         = require('yargs'); 
 
-var argv = require('yargs').argv; 
+var bootstrap_model   = require('./bootstrap/bootstrap_model'); 
 
-console.log(argv); 
+var argv = yargs.argv; 
 
 mongoose.Promise    = global.Promise;
 
@@ -15,88 +13,7 @@ console.log('*************************************');
 console.log('* Welcome to the Node generator App *');
 console.log('*************************************');
 console.log('\n'); 
-console.log('This commands helps you to generate schema'); 
-console.log('You need to answer those questions below to create that new schema'); 
-console.log('\n'); 
-const questions = [
-  {
-    type: 'input',
-    name: 'model_name',
-    message: "What's your model's name ?"
-  },
-  {
-    type: 'input',
-    name: 'property',
-    message: "What's attribute you want to use ?"
-  },
-  {
-    type: 'list',
-    name: 'type',
-    message: "What's type you want to assign him ? ",
-    choices: ['Number', 'String', 'Date', 'Boolean'],
-    default: 'String'
-  }, 
-  {
-    type: 'confirm',
-    name: 'askAgain',
-    message: 'Do you want to add another property ? (just hit enter for YES)',
-    default: true
-  }
-];
 
-var output = {
-  model_name: '', 
-  properties: []
-};
-
-const questionsProp = [
-  {
-    type: 'input',
-    name: 'property',
-    message: "What's attribute you want to use ?"
-  },
-  {
-    type: 'list',
-    name: 'type',
-    message: "What's type you want to assign him ? ",
-    choices: ['Number', 'String', 'Date', 'Boolean'],
-    default: 'String'
-  }, 
-  {
-    type: 'confirm',
-    name: 'askAgain',
-    message: 'Do you want to add another property ? (just hit enter for YES)',
-    default: true
-  }
-];
-
-function ask() {
-  inquirer.prompt(questions).then(answers => {
-    var answer = {property: answers.property, type: answers.type }; 
-    output.model_name = (answers.model_name) ? answers.model_name : "" ; 
-    output.properties.push(answer);
-    if (answers.askAgain) {
-      askProperty();
-    } else {
-      bootstrap.createModelFile(output); 
-    }
-  }).catch((err) => {
-    console.log(err); 
-  });
+if (argv.models ) {
+  bootstrap_model.generateModel(); 
 }
-
-function askProperty() {
-  inquirer.prompt(questionsProp).then(answers => {
-    var answer = {property: answers.property, type: answers.type }; 
-    output.properties.push(answer);
-    if (answers.askAgain) {
-      askProperty();
-    } else {
-      bootstrap.createModelFile(output); 
-    }
-  }).catch((err) => {
-    console.log('Exist prompt', err); 
-  });
-}
-
-ask();

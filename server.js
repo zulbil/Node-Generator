8b92cff45cd0c76ec0ed5a1f97e726ea40af4db2
Joel Alexandre Khang Zulbal <alexkhang25@yahoo.fr>
2019-01-30 23:54:42 +0100

    change command generate model

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const fs            = require('fs');
-const {mongoose}    = require('./config/dbConfig');
+//const {mongoose}    = require('./config/dbConfig');
 const commander     = require('commander');
 const yargs         = require('yargs'); 
 
 var bootstrap_model   = require('./bootstrap/bootstrap_model'); 
 
-var argv = yargs.argv; 
+var argv = yargs
+		   .command('generate model', 'Generate a new model')
+		   .help()
+		   .argv; 
 
-mongoose.Promise    = global.Promise;
+//mongoose.Promise    = global.Promise;
 
 console.log('*************************************');
 console.log('* Welcome to the Node generator App *');
 console.log('*************************************');
 console.log('\n'); 
-
-if (argv.models ) {
+ 
+if (argv._[0] == 'generate' && argv._[1] == 'model' ) {
   bootstrap_model.generateModel(); 
 }

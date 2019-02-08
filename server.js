f14641a015ad7b8a748aab97351733975ead18d2
Joel Alexandre Khang Zulbal <alexkhang25@yahoo.fr>
2019-02-08 14:08:19 +0100

    change if condition

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ console.log('*************************************');
 console.log('* Welcome to the Node generator App *');
 console.log('*************************************');
 console.log('\n'); 
- 
-if (argv._[0] == 'generate' && argv._[1] == 'model' ) {
+
+if (argv._[0] == 'generate' && argv.model == 'model' ) {
   bootstrap_model.generateModel(); 
 }

479eb632b08b0f357ad6d0e3ba70e437b9a51f2a
Joel Alexandre Khang Zulbal <alexkhang25@yahoo.fr>
2019-02-08 14:35:19 +0100

    correction
    Adding generate crud function

---
+++ bootstrap/bootstrap_controller.js
@@ -0,0 +1,13 @@
+const inquirer      = require('inquirer');
+const bootstrap     = require('./bootstrap'); 
+
+function generateCrud () {
+
+console.log('This commands helps you to generate CRUD fonctionnality'); 
+console.log('\n');
+
+}
+
+module.exports = {
+	generateCrud
+}
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const fs            = require('fs');
+const fs            		= require('fs');
 //const {mongoose}    = require('./config/dbConfig');
-const commander     = require('commander');
-const yargs         = require('yargs'); 
+const commander     		= require('commander');
+const yargs         		= require('yargs'); 
 
-var bootstrap_model   = require('./bootstrap/bootstrap_model'); 
+var bootstrap_model   		= require('./bootstrap/bootstrap_model'); 
+var bootstrap_controller 	= require('./bootstrap/bootstrap_controller'); 
 
 var argv = yargs
 		   .command('generate model', 'Generate a new model')
+		   .command('generate crud', 'Generate a CRUD fonctionnality for a specific model')
 		   .help()
 		   .argv; 
 
@@ -20,3 +22,8 @@ console.log('\n');
 if (argv._[0] == 'generate' && argv.model == 'model' ) {
   bootstrap_model.generateModel(); 
 }
+
+if (argv._[0] == 'generate' && argv.model == 'crud' ) {
+  bootstrap_controller.generateCrud(); 
+}
+
